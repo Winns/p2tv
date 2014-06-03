90a665216f96eee9af426ce7aec5e148ba4fdba5
Winns/Pavel <buhhcz@gmail.com>
2014-06-03 12:44:33 +0300

    2.0.26

---
+++ peka2tv_chat2/166081.meta.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name        peka2tv chat
+// @namespace   http://chat.sc2tv.ru
+// @description sc2tv.ru chat with extra features
+// @author      Winns
+// @copyright   27.04.2013, Winns
+// @include     http://chat.sc2tv.ru/*
+// @include     http://sc2tv.ru/*
+// @match 		http://chat.sc2tv.ru/*
+// @match 		http://sc2tv.ru/*
+// @version     2.0.26
+// @updateURL	https://raw.githubusercontent.com/Winns/p2tv/master/peka2tv_chat2/166081.meta.js
+// @downloadURL	https://raw.githubusercontent.com/Winns/p2tv/master/peka2tv_chat2/166081.user.js
+// @grant       GM_addStyle
+// @grant       GM_getValue
+// @grant       GM_setValue
+// @grant       GM_getResourceText
+// @resource    peka2tv_chat_css https://raw.githubusercontent.com/Winns/p2tv/master/peka2tv_chat2/peka2tv_chat.css
+// @run-at		document-end
+// ==/UserScript==
--- peka2tv_chat2/166081.user.js
+++ peka2tv_chat2/166081.user.js
@@ -8,15 +8,14 @@
 // @include     http://sc2tv.ru/*
 // @match 		http://chat.sc2tv.ru/*
 // @match 		http://sc2tv.ru/*
-// @version     2.0.25
-// @updateURL	https://raw.githubusercontent.com/Winns/p2tv/master/peka2tv_chat2/166081.user.js
+// @version     2.0.26
+// @updateURL	https://raw.githubusercontent.com/Winns/p2tv/master/peka2tv_chat2/166081.meta.js
 // @downloadURL	https://raw.githubusercontent.com/Winns/p2tv/master/peka2tv_chat2/166081.user.js
-// @installURL	https://raw.githubusercontent.com/Winns/p2tv/master/peka2tv_chat2/166081.user.js
 // @grant       GM_addStyle
 // @grant       GM_getValue
 // @grant       GM_setValue
 // @grant       GM_getResourceText
-// @resource    peka2tv_chat_css https://raw.github.com/Winns/p2tv/master/peka2tv_chat2/peka2tv_chat.css
+// @resource    peka2tv_chat_css https://raw.githubusercontent.com/Winns/p2tv/master/peka2tv_chat2/peka2tv_chat.css
 // @run-at		document-end
 // ==/UserScript==
 (function () {
